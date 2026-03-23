1421f3f9830b7c3844a98e19fff20302b2f4a91a
Piero Cassano <danilope20050605@gmail.com>
2026-03-23 02:22:43 -0500

    Update Program.cs

--- CineStar/Program.cs
+++ CineStar/Program.cs
@@ -3,17 +3,14 @@ using CineStar.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 Conexion.Inicializar(builder.Configuration);
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddControllers(); // <--- ASEGÚRATE DE QUE ESTO ESTÉ
+builder.Services.AddControllers(); 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
